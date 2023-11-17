17f10af8ebcdc68845286e0567938d2491dd36de
Sandeepa D.S <supundunusingha2@gmail.com>
2023-11-17 09:30:18 +0530

    prescription page (base)

---
+++ lib/features/prescriptions/presentation/prescription_1.dart
@@ -0,0 +1,74 @@
+import 'package:MedInvent/presentation/components/BottomNavBar.dart';
+import 'package:flutter/material.dart';
+
+class Prescriptions extends StatefulWidget {
+  const Prescriptions({super.key});
+
+  @override
+  State<Prescriptions> createState() => _PrescriptionsState();
+}
+
+class _PrescriptionsState extends State<Prescriptions> {
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.topRight,
+                colors: [
+                  Color(0xFF474CA0),
+                  Color(0xFF468FA0),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: AppBar(
+              title: const Text("Prescriptions"),
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              toolbarHeight: screenHeight * 0.1,
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: screenHeight * 0.85,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(screenHeight * 0.06),
+                  topRight: Radius.circular(screenHeight * 0.06),
+                ),
+                color: Colors.white,
+              ),
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //prescriptions
+                ],
+              ),
+            ),
+          ),
+          const Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: BottomNavBar(),
+          ),
+        ],
+      ),
+    );
+  }
+}
