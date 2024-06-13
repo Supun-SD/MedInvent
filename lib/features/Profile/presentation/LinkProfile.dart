867ca6d122d6644db1a9f1909f6efec9c6c08530
IsuraViranga <isuraviranga@gmail.com>
2024-06-14 04:14:53 +0530

    link user api connected

--- lib/features/Profile/presentation/LinkProfile.dart
+++ lib/features/Profile/presentation/LinkProfile.dart
@@ -1,5 +1,8 @@
 import 'package:MedInvent/components/otp_input.dart';
 import 'package:flutter/material.dart';
+import 'package:MedInvent/features/Profile/services/dependent_service.dart';
+import 'package:MedInvent/features/Profile/data/models/linkUser.dart';
+import 'dart:convert';
 
 class AddExistingMember extends StatefulWidget {
   const AddExistingMember({super.key});
@@ -17,7 +20,7 @@ class _AddExistingMemberState extends State<AddExistingMember> {
     final double keyboardSpace = MediaQuery.of(context).viewInsets.bottom;
 
     return SizedBox(
-      height: screenHeight * 0.4 + keyboardSpace,
+      height: screenHeight * 0.5 + keyboardSpace,
       child: PageView(
           controller: _pageController,
           physics: const NeverScrollableScrollPhysics(),
@@ -31,14 +34,83 @@ class _AddExistingMemberState extends State<AddExistingMember> {
   }
 }
 
-class LinkProfile extends StatelessWidget {
+class LinkProfile extends StatefulWidget {
   final PageController pageController;
 
   LinkProfile({super.key, required this.pageController});
 
+  @override
+  State<LinkProfile> createState() => _LinkProfileState();
+}
+
+class _LinkProfileState extends State<LinkProfile> {
   TextEditingController relationship = TextEditingController();
+
   TextEditingController mobileNo = TextEditingController();
+
   TextEditingController nic = TextEditingController();
+
+
+  Future<bool> checkUserAvailabe() async {
+     LinkUser newDepend = LinkUser(
+        mobileNo.text,
+        "",
+        nic.text,
+        relationship.text,
+      );
+      // Send the new member data to the backend
+      try {
+        BaseClient baseClient = BaseClient();
+        var response = await baseClient.postCheckuserdata(
+          '/Notification/check/user/available',
+          newDepend.toRawJson()
+        );
+        if (response != null) {
+          // Handle successful response
+          //print(response);
+          Map<String, dynamic> decodedJson  = json.decode(response);
+          newDepend.receiverID=decodedJson ['data']['userID'];
+          //print(newDepend.receiverID);
+          if(newDepend.receiverID != null) {
+              baseClient = BaseClient();
+              var response = await baseClient.post(
+                  '/Notification/get/tokens/to/send/OTP',
+                  newDepend.toRawJsonForSecondRequest()
+              );
+              if(response != null) {
+                 Map<String, dynamic> decodedJson = json.decode(response);
+                 List<dynamic> data = decodedJson['data'];
+
+                 for (var item in data) {
+                   for (var tokenStore in item['TokenStores']) {
+                     newDepend.FcmTokens.add(tokenStore['fcm_token']);
+                   }
+                 }
+                 if(newDepend.FcmTokens.length>0) {
+                     bool A =await newDepend.temporary();
+                     bool B =await newDepend.assignLoggedUserID();
+                     if(A && B)
+                       {
+                           baseClient = BaseClient();
+                           var response = await baseClient.post(
+                               '/Notification/send/OTP/link/user',
+                               newDepend.toRawJsonForThirdRequest()
+                           );
+                       }
+                 }
+
+              }
+              //print(response);
+          }
+          return true;
+        }
+      } catch (e) {
+        // Handle error
+        print("Error: $e");
+        return false;
+      }
+     return false;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -65,10 +137,15 @@ class LinkProfile extends StatelessWidget {
           height: screenHeight * 0.02,
         ),
         TextButton(
-          onPressed: () {
-            pageController.animateToPage(2,
-                duration: const Duration(milliseconds: 500),
-                curve: Curves.easeInOut);
+          onPressed: () async{
+            //have to do validation before send backend
+            var is_available = await checkUserAvailabe();
+            if(is_available)
+              {
+                widget.pageController.animateToPage(2,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.easeInOut);
+              }
           },
           style: TextButton.styleFrom(
             backgroundColor: const Color(0xFF2980B9),
@@ -118,7 +195,7 @@ class OtpVerify extends StatelessWidget {
         Padding(
             padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.15),
             child: Text(
-              "Enter the confirmation code sent to your relative's mobile number",
+              "Enter the confirmation code sent to your relative's mobile device",
               style: TextStyle(fontSize: screenHeight * 0.015),
               textAlign: TextAlign.center,
             )),
--- lib/features/Profile/services/dependent_service.dart
+++ lib/features/Profile/services/dependent_service.dart
@@ -12,7 +12,7 @@ class BaseClient {
       var response = await client.get(url, headers: _headers);
 
       if (response.statusCode == 200) {
-        print(response.body);
+        //print(response.body);
         return response.body;
       } else {
         throw Exception('Failed to load data. Status code: ${response.statusCode}');
@@ -29,7 +29,7 @@ class BaseClient {
       var url = Uri.parse(baseUrl + api);
       var _headers = {"Content-Type": "application/json"};
       var response = await client.post(url, headers: _headers, body: body);
-
+      print(response.body);
       if (response.statusCode == 200 || response.statusCode == 201) {
         return response.body;
       } else {
@@ -49,6 +49,7 @@ class BaseClient {
       var response = await client.put(url, headers: _headers, body: body);
 
       if (response.statusCode == 200) {
+        print(response);
         return response.body;
       } else {
         throw Exception('Failed to update data. Status code: ${response.statusCode}');
@@ -89,4 +90,23 @@ class BaseClient {
   // Future<dynamic> delete(String api) async {
   //   // Implement the DELETE method if needed
   // }
+
+  //Link User service api
+
+  Future<dynamic> postCheckuserdata(String api, dynamic body) async {
+    try {
+      var url = Uri.parse(baseUrl + api);
+      var _headers = {"Content-Type": "application/json"};
+      var response = await client.post(url, headers: _headers, body: body);
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return response.body;
+      } else {
+        throw Exception('Failed to post data. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error during HTTP request: $e');
+    } finally {
+      client.close();
+    }
+  }
 }
