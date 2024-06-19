9c230a710e28a0d6d41e478681ef0d41fe65b22b
Supun D <supundunusingha2@gmail.com>
2024-06-20 02:38:41 +0530

    daily medications

--- lib/components/medication_card.dart
+++ lib/components/medication_card.dart
@@ -1,182 +1,109 @@
+import 'package:MedInvent/features/Daily_medication/Presentation/daily_medication.dart';
 import 'package:flutter/material.dart';
+import '../features/Daily_medication/models/DailyMedication.dart';
 
-class Medication_card extends StatelessWidget {
-  final double screenHeight;
-  final double screenWidth;
-  final String medication1;
-  final String medication2;
-  final String User;
-  final color;
+class MedicationCard extends StatelessWidget {
+  final List<DailyMedication> dailyMedications;
 
-  const Medication_card({
-    Key? key,
-    required this.screenHeight,
-    required this.screenWidth,
-    required this.medication1,
-    required this.medication2,
-    required this.User,
-    required this.color,
-  }) : super(key: key);
+  const MedicationCard({Key? key, required this.dailyMedications})
+      : super(key: key);
+
+  int calculateTotalMedicationIntake(DailyMedication dailyMedication) {
+    int totalIntakes = 0;
+    for (var medicine in dailyMedication.presMedicine) {
+      totalIntakes += medicine.medicationIntake.length;
+    }
+    return totalIntakes;
+  }
+
+  int calculateTakenMedicationIntakes(DailyMedication dailyMedication) {
+    int takenIntakes = 0;
+    for (var medicine in dailyMedication.presMedicine) {
+      for (var intake in medicine.medicationIntake) {
+        if (intake.taken) {
+          takenIntakes++;
+        }
+      }
+    }
+    return takenIntakes;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    return InkWell(
+      onTap: () => {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  DailyMedicationDetails(dailyMedications: dailyMedications)),
+        )
+      },
       child: Container(
-        margin: EdgeInsets.symmetric(vertical: screenHeight * 0.01),
-        padding: const EdgeInsets.all(20),
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(
+            vertical: screenHeight * 0.025, horizontal: screenWidth * 0.08),
+        margin: EdgeInsets.symmetric(horizontal: screenWidth * 0.08),
         decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
           boxShadow: [
             BoxShadow(
-              color: Colors.grey.withOpacity(0.15),
+              color: Colors.grey.withOpacity(0.2),
               blurRadius: 10,
             ),
           ],
-          color: color,
-          borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              '$User Daily Medications',
-            ),
-            const SizedBox(height: 20),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                color: Colors.white,
+            const Center(
                 child: Text(
-                  "$medication1 Medication",
-                  style: const TextStyle(
-                      fontSize: 11, fontWeight: FontWeight.bold),
-                ),
-              ),
+              "Your daily medications",
+              style: TextStyle(fontSize: 16),
+            )),
+            const Divider(),
+            const SizedBox(
+              height: 10,
             ),
-            const SizedBox(height: 5),
-            Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.1),
-                        spreadRadius: screenHeight * 0.01,
-                        blurRadius: screenHeight * 0.25,
-                      ),
-                    ],
-                    color: color,
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: const StepProgressIndicator(
-                    totalSteps: 12,
-                    currentStep: 6,
-                    size: 25,
-                    padding: 0,
-                    selectedColor: Colors.green,
-                    unselectedColor: Colors.grey,
-                    roundedEdges: Radius.circular(10),
-                    selectedGradientColor: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [
-                        Color.fromARGB(150, 63, 195, 128),
-                        Colors.black12
-                      ],
-                    ),
-                    unselectedGradientColor: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: [Colors.black12, Colors.black12],
-                    ),
-                  ),
-                )
-              ],
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Icon(
-                      Icons.watch_later_outlined,
-                      size: 10,
-                    ),
-                    Text("8.00AM", style: TextStyle(fontSize: 11)),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Icon(Icons.watch_later_outlined, size: 10),
-                    Text("1.00PM", style: TextStyle(fontSize: 11)),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.watch_later_outlined,
-                      size: 10,
-                    ),
-                    Text("8.00PM", style: TextStyle(fontSize: 11)),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Container(
-                color: Colors.white,
-                child: Text(
-                  "$medication2 Medication",
-                  style: const TextStyle(
-                      fontSize: 11, fontWeight: FontWeight.bold),
-                ),
-              ),
-            ),
-            const SizedBox(height: 5),
-            const StepProgressIndicator(
-              totalSteps: 2,
-              currentStep: 2,
-              size: 25,
-              padding: 0,
-              selectedColor: Colors.green,
-              unselectedColor: Colors.grey,
-              roundedEdges: Radius.circular(10),
-              selectedGradientColor: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [Color.fromARGB(150, 63, 195, 128), Colors.black12],
-              ),
-              unselectedGradientColor: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [Colors.black12, Colors.black12],
-              ),
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    Icon(
-                      Icons.watch_later_outlined,
-                      size: 10,
-                    ),
-                    Text("8.00AM", style: TextStyle(fontSize: 11)),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.watch_later_outlined,
-                      size: 10,
-                    ),
-                    Text("8.00PM", style: TextStyle(fontSize: 11)),
-                  ],
-                ),
-              ],
-            ),
+            ...dailyMedications
+                .map((dailyMed) => PrescriptionTemplate(
+                    dailyMedication: dailyMed,
+                    totalMeds: calculateTotalMedicationIntake(dailyMed),
+                    takenMeds: calculateTakenMedicationIntakes(dailyMed)))
+                .toList(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class PrescriptionTemplate extends StatelessWidget {
+  const PrescriptionTemplate(
+      {super.key,
+      required this.dailyMedication,
+      required this.totalMeds,
+      required this.takenMeds});
+  final DailyMedication dailyMedication;
+  final int totalMeds;
+  final int takenMeds;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(dailyMedication.presName),
+          const SizedBox(
+            height: 8,
+          ),
+          StepProgressIndicator(totalSteps: totalMeds, currentStep: takenMeds)
+        ],
       ),
     );
   }
@@ -185,29 +112,11 @@ class Medication_card extends StatelessWidget {
 class StepProgressIndicator extends StatelessWidget {
   final int totalSteps;
   final int currentStep;
-  final double size;
-  final double padding;
-  final Color selectedColor;
-  final Color unselectedColor;
-  final Radius roundedEdges;
-  final Gradient selectedGradientColor;
-  final Gradient unselectedGradientColor;
 
   const StepProgressIndicator({
     super.key,
     required this.totalSteps,
     required this.currentStep,
-    this.size = 2,
-    this.padding = 2,
-    this.selectedColor = Colors.blue,
-    this.unselectedColor = Colors.grey,
-    this.roundedEdges = const Radius.circular(0),
-    this.selectedGradientColor = const LinearGradient(
-      colors: [Colors.blue, Colors.green],
-    ),
-    this.unselectedGradientColor = const LinearGradient(
-      colors: [Colors.grey, Colors.grey],
-    ),
   });
 
   @override
@@ -216,27 +125,35 @@ class StepProgressIndicator extends StatelessWidget {
       children: [
         Container(
           width: 300,
-          height: size,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(roundedEdges),
-            gradient: unselectedGradientColor,
+          height: 25,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.black12, Colors.black12],
+            ),
           ),
         ),
         Container(
-          width: size * currentStep,
-          height: size,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(roundedEdges),
-            gradient: selectedGradientColor,
+          width: currentStep * 25,
+          height: 25,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Color.fromARGB(150, 63, 195, 128), Colors.black12],
+            ),
           ),
         ),
         Positioned(
-          left: size * (currentStep - 1),
+          left: 25 * (currentStep - 1),
           child: Container(
-            width: size,
-            height: size,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(roundedEdges),
+            width: 25,
+            height: 25,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
               color: Colors.transparent,
             ),
             child: const Icon(
--- lib/features/Daily_medication/Presentation/daily_medication.dart
+++ lib/features/Daily_medication/Presentation/daily_medication.dart
@@ -1,214 +1,239 @@
+import 'package:MedInvent/providers/dailyMedicationsProvider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import '../models/DailyMedication.dart';
 
-class DailyMed extends StatefulWidget {
-  const DailyMed({super.key});
+class DailyMedicationDetails extends StatelessWidget {
+  const DailyMedicationDetails({super.key, required this.dailyMedications});
+  final List<DailyMedication> dailyMedications;
 
-  @override
-  State<DailyMed> createState() => _DailyMedState();
-}
-
-class _DailyMedState extends State<DailyMed> {
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
     final double screenHeight = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF474CA0),
+                Color(0xFF468FA0),
+              ],
+            ),
+          ),
+        ),
+        title: const Text("Daily medication details"),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF474CA0),
+              Color(0xFF468FA0),
+            ],
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.only(top: screenHeight * 0.025),
             decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.topRight,
-                colors: [
-                  Color(0xFF474CA0),
-                  Color(0xFF468FA0),
+                color: Color(0xFFfafafa),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50.0),
+                  topRight: Radius.circular(50.0),
+                )),
+            child: Container(
+              width: double.infinity,
+              margin: EdgeInsets.symmetric(
+                  horizontal: screenWidth * 0.07,
+                  vertical: screenHeight * 0.05),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ...dailyMedications
+                      .map((dailyMed) => Pres(dailyMedication: dailyMed))
+                      .toList(),
                 ],
               ),
             ),
           ),
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            child: AppBar(
-              title: const Text('Daily Medications'),
-              elevation: 0,
-              backgroundColor: Colors.transparent,
-              toolbarHeight: screenHeight * 0.1,
-            ),
+        ),
+      ),
+    );
+  }
+}
+
+class Pres extends StatelessWidget {
+  const Pres({super.key, required this.dailyMedication});
+  final DailyMedication dailyMedication;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+      margin: const EdgeInsets.only(bottom: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.15),
+            spreadRadius: 5,
+            blurRadius: 7,
           ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              height: screenHeight * 0.85,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(screenHeight * 0.06),
-                  topRight: Radius.circular(screenHeight * 0.06),
-                ),
-                color: Colors.white,
-              ),
-            ),
-          ),
-          const Positioned(
-            top: 200,
-            left: 50,
-            child: Text(
-              'Fever  Prescription',
-              style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
-            ),
-          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+              child: Text(
+            dailyMedication.presName,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+          )),
+          const Divider(),
           const SizedBox(
-            height: 20.0,
+            height: 20,
           ),
-          Container(
-            width: 350.0,
-            height: 350.0,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(30),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.4),
-                  spreadRadius: screenHeight * 0.02,
-                  blurRadius: screenHeight * 0.1,
-                ),
-              ],
-            ),
-            margin: const EdgeInsets.fromLTRB(20.0, 250.0, 20.0, 210),
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20.0,
-              vertical: 10.0,
-            ),
-            child: Column(
-              children: [
-                const CustomWidget(),
-                const SizedBox(
-                  height: 2.0,
-                ),
-                const Divider(
-                  height: 3.0,
-                  color: Colors.black12,
-                ),
-                const CustomWidget(),
-                const SizedBox(
-                  height: 2.0,
-                ),
-                Divider(
-                  height: 2.0,
-                  color: Colors.blue[700],
-                ),
-                const CustomWidget(),
-                const SizedBox(
-                  height: 2.0,
-                ),
-                Container(
-                  padding: const EdgeInsets.all(0.5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    color: Colors.white,
-                  ),
-                  child: TextButton(
-                    onPressed: () {},
-                    style: TextButton.styleFrom(
-                        side: const BorderSide(
-                          color: Colors.black,
-                          width: 0.5,
-                        ),
-                        fixedSize: const Size(80.0, 5.0)),
-                    child: const Text(
-                      'More Details',
-                      style: TextStyle(
-                        fontSize: 10.0,
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          ...dailyMedication.presMedicine
+              .map((med) => Med(medicine: med))
+              .toList()
         ],
       ),
     );
   }
 }
 
-class CustomWidget extends StatefulWidget {
-  const CustomWidget({super.key});
+class Med extends StatelessWidget {
+  const Med({super.key, required this.medicine});
+  final Medicine medicine;
 
-  @override
-  State<CustomWidget> createState() => Card();
-}
-
-
-class Card extends State<CustomWidget> {
-  final borderRadius = BorderRadius.circular(5.0);
-  int numberOfTablets = 0;
-  int daysLeft = 0;
-  String nameOfMed = 'Panadol';
-  bool isChecked = false;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          children: [
-            Container(
-              padding: const EdgeInsets.all(0.6),
-              decoration: BoxDecoration(
-                  color: Colors.black12, borderRadius: borderRadius),
-              child: ClipRRect(
-                borderRadius: borderRadius,
-                child: SizedBox.fromSize(
-                  size: const Size.fromRadius(20),
-                  child: Image.asset(
-                    'assets/images/med.jpeg',
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(width: 7.8),
-            Column(
-              children: [
-                Text(
-                  '$nameOfMed * $numberOfTablets',
-                  style: const TextStyle(fontSize: 9.0),
-                ),
-                const SizedBox(height: 2.0),
-                Container(
-                  padding: const EdgeInsets.all(5.0),
-                  decoration: BoxDecoration(
-                      color: Colors.green,
-                      borderRadius: BorderRadius.circular(10.0)),
-                  child: Text(
-                    ' $daysLeft days left ',
-                    style: const TextStyle(color: Colors.white, fontSize: 9.0),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(width: 130.0),
-            const Text(
-              'Time',
-              style: TextStyle(
-                fontSize: 10.0,
-              ),
-            ),
-            Checkbox(
-                value: isChecked,
-                onChanged: (bool? value) {
-                  setState(() {
-                    isChecked = value!;
-                  });
-                })
-          ],
+    return Container(
+        padding: const EdgeInsets.all(15),
+        margin: const EdgeInsets.only(bottom: 8),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: const Color(0xFFe3f6ff),
+          borderRadius: BorderRadius.circular(10),
         ),
-      ],
-    );
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "${medicine.name} (quantity : ${medicine.qty})",
+              style: const TextStyle(fontSize: 15),
+            ),
+            const Divider(
+              color: Colors.blue,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  ...medicine.medicationIntake
+                      .map((intake) => Marker(medicationIntake: intake))
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
 
+class Marker extends ConsumerStatefulWidget {
+  const Marker({super.key, required this.medicationIntake});
+  final MedicationIntake medicationIntake;
+
+  @override
+  ConsumerState<Marker> createState() => _MarkerState();
+}
+
+class _MarkerState extends ConsumerState<Marker> {
+  bool isLoading = false;
+
+  String formatTime(String timeString) {
+    List<String> parts = timeString.split(':');
+    int hours = int.parse(parts[0]);
+
+    String period = hours < 12 ? 'AM' : 'PM';
+
+    if (hours > 12) {
+      hours -= 12;
+    } else if (hours == 0) {
+      hours = 12;
+    }
+
+    return '$hours:${parts[1]} $period';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(right: 8),
+      margin: const EdgeInsets.only(right: 5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        border: Border.all(
+          color: widget.medicationIntake.taken ? Colors.green : Colors.grey,
+          width: 1.0,
+        ),
+      ),
+      child: Row(
+        children: [
+          if (isLoading)
+            const Padding(
+              padding: EdgeInsets.all(13.0),
+              child: SpinKitCircle(
+                size: 15,
+                color: Colors.blue,
+              ),
+            )
+          else
+            widget.medicationIntake.taken
+                ? const Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Icon(
+                      Icons.check_circle,
+                      color: Colors.green,
+                    ))
+                : Checkbox(
+                    value: widget.medicationIntake.taken,
+                    onChanged: (bool? value) async {
+                      setState(() {
+                        isLoading = true;
+                      });
+
+                      try {
+                        await ref
+                            .read(dailyMedicationsProvider.notifier)
+                            .updateDailyMedication(widget.medicationIntake.id,
+                                widget.medicationIntake);
+                        setState(() {
+                          widget.medicationIntake.taken = value ?? false;
+                          isLoading = false;
+                        });
+                      } catch (e) {
+                        setState(() {
+                          isLoading = false;
+                        });
+                      }
+                    },
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    visualDensity: VisualDensity.standard,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+          Text(formatTime(widget.medicationIntake.time)),
+        ],
+      ),
+    );
+  }
+}
